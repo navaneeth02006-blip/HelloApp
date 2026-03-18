f4f1642be0615ad35e57041c554215b2cb4e47aa
navaneeth02006-blip <navaneeth02006@gmail.com>
2026-03-18 13:20:37 +0530

    UC3 implementation completed

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -1,9 +1,10 @@
 public class HelloApp {
     public static void main(String[] args) {
-        if (args.length >= 1 && args[0] != null && !args[0].trim().isEmpty()) {
-            System.out.println("Hello " + args[0].trim());
-        } else {
-            System.out.println("Hello World");
+        String name = "World";
+        if (args.length > 0) {
+            name = args[0];
         }
+        System.out.println("Hello"+" "+ name +" "+ "!");
     }
+    
 }
