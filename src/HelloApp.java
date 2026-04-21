6433d539b3b3b78ee7a0954d20d7dbd52b8ea90c
navaneeth02006-blip <navaneeth02006@gmail.com>
2026-04-21 09:22:35 +0530

    UC7 implementation completed

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -2,22 +2,15 @@ public class HelloApp {
 
     public static void main(String[] args) {
 
+        String greeting;
+
         if (args.length == 0) {
-            System.out.println("Hello, World!");
-            return;
+            greeting = "Hello, World!";
+        } else {
+            String names = String.join(", ", args);
+            greeting = "Hello, " + names + "!";
         }
 
-        StringBuilder nameBuilder = new StringBuilder();
-
-        for (String name : args) {
-            nameBuilder.append(name).append(", ");
-        }
-
-        String names = "";
-        if (nameBuilder.length() > 0) {
-            names = nameBuilder.substring(0, nameBuilder.length() - 2);
-        }
-
-        System.out.println("Hello, " + names + "!");
+        System.out.println(greeting);
     }
 }
